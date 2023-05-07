216b115312054e99e406cfeedad615a186ba546c
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-05-07 00:34:50 -0300

    adicionando classe de tratamento de erros, @exceptionHandler

---
+++ src/main/java/com/remedios/eder/curso/remedios/infra/TratadorDeErros.java
@@ -0,0 +1,16 @@
+package com.remedios.eder.curso.remedios.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<?> tratador404() {
+		return ResponseEntity.notFound().build();
+	}
+
+}
--- src/main/java/com/remedios/eder/curso/remedios/remedio/Laboratorio.java
+++ src/main/java/com/remedios/eder/curso/remedios/remedio/Laboratorio.java
@@ -20,5 +20,6 @@ public enum Laboratorio {
 	SANDOZ,
 	NATULAB
 	
+	
 
 }

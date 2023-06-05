5171f0d38528704d7eea4e316719cc4f6f882f8c
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-06-04 23:12:49 -0300

    concluindo configuracao de autenticacao de usuario

--- src/main/java/com/remedios/eder/curso/remedios/usuarios/Usuario.java
+++ src/main/java/com/remedios/eder/curso/remedios/usuarios/Usuario.java
@@ -1,5 +1,12 @@
 package com.remedios.eder.curso.remedios.usuarios;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,12 +24,51 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String login;
 	private String senha;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// IMPLEMENTA  CONTROLE DE ACESSO, SERA VISTO DEPOIS
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return senha;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return login;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	
 	
 

eef7671b58045c6a470da314cc8d04bbf6b43b33
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-06-30 22:12:45 -0300

    recuperando o token no filtro de seguranca

--- src/main/java/com/remedios/eder/curso/remedios/util/SecurityFilter.java
+++ src/main/java/com/remedios/eder/curso/remedios/util/SecurityFilter.java
@@ -14,11 +14,23 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SecurityFilter extends OncePerRequestFilter {
 // um filtro por requisição  tradução da classe herdada acima
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-			FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+
+		var tokenJWT = recuperarToken(request);
+		System.out.println(tokenJWT);
+
 		filterChain.doFilter(request, response);
-		
+
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		var autorizacaoHeader = request.getHeader("Authorization");
+		if (autorizacaoHeader == null) {
+			throw new RuntimeException("Token não enviado!");
+		}
+		return autorizacaoHeader;
 	}
 
 }
